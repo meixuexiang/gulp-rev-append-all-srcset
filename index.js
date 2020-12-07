bfdc4a0c3ab9f0bccc4191eeedc548c099cfc983
meixuexiang <meixuexiang@gmail.com>
2020-12-08 00:12:19 +0800

    support options.length

--- index.js
+++ index.js
@@ -13,18 +13,20 @@ function getDependencyPath(pathString, file, isNgRoute) {
   return normPath.startsWith(path.sep) ? path.join(file.base, normPath) : path.resolve(path.dirname(file.path), isNgRoute ? '../' + normPath : normPath)
 }
 
-function getHexCode(dependencyPath) {
+function getHexCode(dependencyPath, length) {
   try {
     const data = fs.readFileSync(dependencyPath);
     const hash = crypto.createHash('md5');
     hash.update(data.toString(), 'utf8');
-    return hash.digest('hex');
+    return hash.digest('hex').slice(0, length);
   } catch (error) {
     return '';
   }
 }
 
-const revPlugin = function revPlugin() {
+const revPlugin = function revPlugin(options) {
+  options = options || {};
+  options.length = options.length || 32;
   return map(function (file, cb) {
     if (!file) {
       throw new PluginError('gulp-rev-append', 'Missing file option for gulp-rev-append.');
@@ -35,7 +37,7 @@ const revPlugin = function revPlugin() {
     const isNgRoute = file.path.includes('/app.js');
     const contents = file.contents.toString().replace(FILE_DECL, (txt, attr, quote, pathString) => {
       const dependencyPath = getDependencyPath(pathString, file, isNgRoute);
-      const code = getHexCode(dependencyPath);
+      const code = getHexCode(dependencyPath, options.length);
       return code ? `${attr}${quote}${pathString}?v=${code}${quote}` : txt;
     }).replace(FILE_DECL_SRCSET, (attrTxt) => {
       return 'srcset="' + attrTxt.slice('srcset="'.length, -1).trim().split(/\x20*,\x20*/).map((srcTxt) => {
@@ -44,7 +46,7 @@ const revPlugin = function revPlugin() {
         descriptor = descriptor.trim();
         if (pathString && descriptor) {
           const dependencyPath = getDependencyPath(pathString, file, isNgRoute);
-          const code = getHexCode(dependencyPath);
+          const code = getHexCode(dependencyPath, options.length);
           if (code) {
             return `${pathString}?v=${code} ${descriptor}`;
           }
